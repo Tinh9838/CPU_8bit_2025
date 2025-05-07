d1fc9ae75f1fa540135d3a259b6a13a9a324a91b
Tinh9838 <tinh9838@gmail.com>
2025-05-07 21:28:39 +0700

    Create ROM.v

---
+++ ROM/ROM.v
@@ -0,0 +1,38 @@
+//   +ROM cung cấp chỉ-lệnh cho CPU thực thi, không thay đổi trong lúc chạy
+//
+//   +ROM thường dùng initial + $readmemh(...) để nạp lệnh ban đầu
+//
+//
+//   + luồng : addr -> instruction
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+module rom (
+    input  wire [7:0] addr,          // địa chỉ chương trình
+    output reg  [7:0] instruction    // lệnh đầu ra
+);
+
+    // Bộ nhớ chương trình (ROM)
+    reg [7:0] rom_mem [0:255];
+
+    // Load dữ liệu từ file (chỉ dùng khi mô phỏng hoặc preload trên FPGA)
+    initial begin
+        $readmemh("program.hex", rom_mem);  // bạn có thể đổi sang .mem nếu cần
+    end
+
+    always @(*) begin
+        instruction = rom_mem[addr];
+    end
+
+endmodule
